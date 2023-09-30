aaa00b316de89c4fe02af0ed97d0e288ff51fc20
stanleywanjau <stanleywanjau371@gmail.com>
2023-09-30 16:07:00 -0700

    completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+  console.log(add(a,b))
+}
+function subtract(a,b){
+  console.log(subtract(a,b))
+}
+function multiply(a,b){
+  console.log(multiply(a,b))
+}
+function divide(a,b){
+  console.log(divide(a,b))
+}
+function add(a,b){
+  return a+b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
+function increment(a){
+  return a+1
+}
+function decrement(a){
+  return a-1
+}
+function makeInt(n){
+  return parseInt(n,10)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
